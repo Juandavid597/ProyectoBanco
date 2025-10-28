406685f5a985317ec4878cac798eb80083bbf3bc
Jdperez597 <chuganeythor777@gmail.com>
2025-10-27 21:00:22 -0500

    modificaciones en la carpeta DTO
    Se añadieron las validaciones de los datos de entrada en los DTO's

--- demo/src/main/java/com/example/demo/dto/MovimientoDto.java
+++ demo/src/main/java/com/example/demo/dto/MovimientoDto.java
@@ -1,8 +1,10 @@
 package com.example.demo.dto;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,13 +13,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MovimientoDto {
-    private UUID id;
+    
+    @NotBlank(message = "El tipo de movimiento es obligatorio (por ejemplo, 'Depósito' o 'Retiro')")
     private String tipo;
+
+    @Positive(message = "El monto del movimiento debe ser mayor que cero")
+    @NotNull(message = "El monto no debe de estar vacio")
     private double monto;
+
+    @Min(value = 0, message = "El saldo antes del movimiento no puede ser negativo")
     private double saldoAntes;
+
+    @Min(value = 0, message = "El saldo después del movimiento no puede ser negativo")
     private double saldoDespues;
+
     private String descripcion;
-    private LocalDateTime fecha;
-    private String referencia;
+
+    @NotBlank(message = "La cuenta relacionada es obligatoria")
     private String cuentaRelacionada;
 }
