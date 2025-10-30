02a73cd5751c5cc64f68fdaa2d79ec4362fde623
Jdperez597 <chuganeythor777@gmail.com>
2025-10-29 21:40:59 -0500

    bancoController modis 1
    Se hicieron modificaciones en bancoController, se crearon 2 metodos

--- demo/src/main/java/com/example/demo/controllers/BancoController.java
+++ demo/src/main/java/com/example/demo/controllers/BancoController.java
@@ -1,11 +1,71 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.BancoDto;
+import com.example.demo.dto.ClienteDto;
+import com.example.demo.entity.Cliente;
+import com.example.demo.helpers.ResponseHelper;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/banco")
 public class BancoController {
-    
-public static void main(String[] args) {
-    
-    System.out.println("sdjhsznb");
+    List<Cliente> clientes = new ArrayList<>();
 
-}
+    @GetMapping
+    public ResponseEntity<?> listarClientes() {
+        try {
+            return ResponseHelper.response(HttpStatus.OK, true, clientes, "Este metodo debe mostrar la lista de clientes");
+        } catch (Exception e) {
+            return ResponseHelper.catchResponse(e);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarCliente(@PathVariable String id) {
+        try {
+            Cliente clienteFound = clientes.stream().filter(item->item.getId().equals(UUID.fromString(id))).findFirst().orElse(null);
+            if (clienteFound==null) {
+                return ResponseHelper.response(HttpStatus.NOT_FOUND, false, 
+                "", "Cliente no encontrado");
+            }
+            return ResponseHelper.response(HttpStatus.OK, true, clienteFound, "Esta ruta muestra la información detallada del cliente");
+
+        } catch (Exception e) {
+            return ResponseHelper.catchResponse(e);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crearCliente(@Valid @RequestBody ClienteDto clientes, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return ResponseHelper.validFields(result);
+        }
+
+        try {
+
+            Cliente clienteNuevo = new Cliente(UUID.randomUUID(), ) 
+            return ResponseHelper.response(HttpStatus.OK, true, clientes, "Este metodo debe mostrar la lista de clientes");
+        } catch (Exception e) {
+            return ResponseHelper.catchResponse(e);
+        }
+    }
+
 }
