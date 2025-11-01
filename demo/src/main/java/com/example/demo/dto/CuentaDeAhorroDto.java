2733f5760d7b830aadd6c2f7e0f2d3e4dc70bacb
marlyyn97-prog <marlyyn97@gmail.com>
2025-11-01 11:29:26 -0500

    entidadClientesInstanciar

--- demo/src/main/java/com/example/demo/dto/CuentaDeAhorroDto.java
+++ demo/src/main/java/com/example/demo/dto/CuentaDeAhorroDto.java
@@ -20,18 +20,10 @@ public class CuentaDeAhorroDto {
 
     // Saldo inicial ≥ $10.000 = OK
 
-    @NotBlank(message = "El número de cuenta es obligatorio")
-    private String numeroCuenta;
-
-    @NotNull(message = "El propietario no puede ser nulo")
-    private Cliente propietario;
-
-    @Min(value = 0, message = "El saldo no puede ser negativo")
+    
+    @NotNull(message = "El saldo inicial es obligatorio")
+    @Min(value = 10000, message = "El saldo debe ser mayor a $10.000")
     private double saldo;
-
-    @NotNull(message = "La fecha de creación es obligatoria")
-    @PastOrPresent(message = "La fecha de creación no puede ser futura")
-    private LocalDate fechaCreacion;
 
     @NotBlank(message = "El tipo de cuenta es obligatorio")
     private String tipoCuenta;
