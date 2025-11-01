4315a28b1fabc41de0865b9360bcb23f5ed6638e
JimySi04 <jimysierra20024@gmail.com>
2025-11-01 11:30:01 -0500

    Modificaciones de optimización, y creacion de listas
    Se quitan decoradores y se crean listas vacias en constructor de banco.

--- demo/src/main/java/com/example/demo/controllers/BancoController.java
+++ demo/src/main/java/com/example/demo/controllers/BancoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.Banco;
+
 import com.example.demo.entity.Cliente;
 import com.example.demo.helpers.ResponseHelper;
 
--- demo/src/main/java/com/example/demo/entity/CuentaAhorros.java
+++ demo/src/main/java/com/example/demo/entity/CuentaAhorros.java
@@ -4,12 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class CuentaAhorros {
     
