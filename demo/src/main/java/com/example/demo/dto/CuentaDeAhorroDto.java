d8f6a16b32a176c69de81e646f78b1407fd22321
JimySi04 <jimysierra20024@gmail.com>
2025-11-07 20:45:50 -0500

    cambio en cuenta de ahorro dto
    para validación de cuenta de ahorro

--- demo/src/main/java/com/example/demo/dto/CuentaDeAhorroDto.java
+++ demo/src/main/java/com/example/demo/dto/CuentaDeAhorroDto.java
@@ -9,6 +9,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,5 +27,6 @@ public class CuentaDeAhorroDto {
     private double saldo;
 
     @NotBlank(message = "El tipo de cuenta es obligatorio")
+    @Pattern(regexp = "ahorro|corriente", flags = Pattern.Flag.CASE_INSENSITIVE, message = "El tipo de cuenta debe ser 'ahorro' o 'corriente'")
     private String tipoCuenta;
 }
