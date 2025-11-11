972e3a2debf736e53fbbf9d5bcfacf11c6a456d5
JimySi04 <jimysierra20024@gmail.com>
2025-11-10 21:33:47 -0500

    Se creó método crear cuenta
    se crea cuenta de ahorros, se cambia la entidad para que no se le asigne un cliente, si no que se le asigne el nombre del cliente y su documento

--- demo/src/main/java/com/example/demo/entity/CuentaAhorros.java
+++ demo/src/main/java/com/example/demo/entity/CuentaAhorros.java
@@ -14,7 +14,8 @@ import lombok.NoArgsConstructor;
 public class CuentaAhorros {
     
     private UUID id;
-    private Cliente propietario;
+    private String nombrePropietario;
+    private String documentoPropietario;
     private double saldo;
     private List<Movimiento> movimientos;
     private LocalDate fechaCreacion;
@@ -24,15 +25,15 @@ public class CuentaAhorros {
 
 
 
-    public CuentaAhorros(String numeroCuenta, Cliente propietario, double saldo, LocalDate fechaCreacion,
-            boolean activa, String tipoCuenta) {
+    public CuentaAhorros(String nombrePropietario, String documentoPropietario, double saldo,String tipoCuenta, boolean activa) {
 
         this.id = UUID.randomUUID();
         this.fechaCreacion = LocalDate.now();
         this.numeroCuenta = "ACC-" + id.toString().replaceAll("[^\\d]", "").substring(0, 10);
         this.movimientos = new ArrayList<>();
         
-        this.propietario = propietario;
+        this.nombrePropietario = nombrePropietario;
+        this.documentoPropietario = documentoPropietario;
         this.saldo = saldo;
         this.activa = activa;
         this.tipoCuenta = tipoCuenta;
