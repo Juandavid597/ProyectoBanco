c3eeddbc4feb1410632f6aa592f0ec88e125e67b
JimySi04 <jimysierra20024@gmail.com>
2025-11-13 21:45:39 -0500

    creación de movimientos
    Se crea método para depositar dinero, y se corrigen cosas de la entidad y el constructor de "Movimiento"

--- demo/src/main/java/com/example/demo/controllers/MovimientoController.java
+++ demo/src/main/java/com/example/demo/controllers/MovimientoController.java
@@ -1,5 +1,70 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.CuentaDeAhorroDto;
+import com.example.demo.dto.MovimientoDto;
+import com.example.demo.entity.Banco;
+import com.example.demo.entity.Cliente;
+import com.example.demo.entity.CuentaAhorros;
+import com.example.demo.entity.Movimiento;
+import com.example.demo.helpers.ResponseHelper;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/Movimiento")
+@CrossOrigin("*")
 public class MovimientoController {
+    private final Banco fakeDb = Banco.getInstancia();
+
+
+    @PostMapping("/{numeroCuenta}")
+    public ResponseEntity<?> depositarDinero(@PathVariable String numeroCuenta, @Valid @RequestBody MovimientoDto MovimientoActualizar, BindingResult result){
+
+        if (result.hasErrors()){
+            return ResponseHelper.validFields(result);
+        }
+
+        try{
+
+            //Validar si existe la cuenta solicitada por numero de cuenta
+            
+            CuentaAhorros cuentaFound = fakeDb.getCuentas().stream().filter(item->item.getNumeroCuenta().equals(numeroCuenta)).findFirst().orElse(null);
+
+            if (cuentaFound == null) {
+                return ResponseHelper.response(HttpStatus.NOT_FOUND, false, "", "No se encuentra una cuenta asociada con el número de cuenta ingresada");
+            }
+
+            
     
+            //Si el cliente no tiene una cuenta procedemos a crear la respectiva cuenta.
+
+            Movimiento newDeposito= new Movimiento(MovimientoActualizar.getTipo(), MovimientoActualizar.getMonto(), cuentaFound.getSaldo(), MovimientoActualizar.getDescripcion(), cuentaFound.getNumeroCuenta());
+
+
+            newDeposito.setSaldoDespues(newDeposito.getSaldoAntes()-newDeposito.getMonto());
+
+            cuentaFound.getMovimientos().add(newDeposito);
+
+            return ResponseHelper.response(HttpStatus.OK, true, newDeposito, "Se ha hecho el deposito correctamente");
+
+        }
+
+        catch (Exception e){
+            return ResponseHelper.catchResponse(e);
+        }
+    }
+
 }
--- demo/src/main/java/com/example/demo/entity/Movimiento.java
+++ demo/src/main/java/com/example/demo/entity/Movimiento.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -19,21 +20,21 @@ public class Movimiento {
     private double saldoAntes;
     private double saldoDespues;
     private String descripcion;
-    private LocalDateTime fecha;
+    private LocalDate fecha;
     private String referencia;
     private String cuentaRelacionada;
 
 
-    public Movimiento(String tipo, double monto, double saldoAntes, double saldoDespues, String descripcion, LocalDateTime fecha, String referencia, String cuentaRelacionada) {
+    public Movimiento(String tipo, double monto, double saldoAntes, String descripcion, String cuentaRelacionada) {
 
         this.id = UUID.randomUUID();      
         this.tipo = tipo;
         this.monto = monto;
         this.saldoAntes = saldoAntes;
-        this.saldoDespues = saldoDespues;
+        this.saldoDespues = 0;
         this.descripcion = descripcion;
-        this.fecha = fecha;
-        this.referencia = referencia;
+        this.fecha = LocalDate.now();
+        this.referencia = "PAY-" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
         this.cuentaRelacionada = cuentaRelacionada;
     }
 
